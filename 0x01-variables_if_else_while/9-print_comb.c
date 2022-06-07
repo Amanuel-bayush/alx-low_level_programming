3421b9ee2e80cbae0a9d3ba5f849e1c03f5a6446
Amanuel-bayush <bantelaybr@gmail.com>
2022-06-06 18:43:59 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,6 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,10 +13,10 @@ int main(void)
 		putchar(ch);
 		if (ch != 57)
 		{ 
-			putchar("44");
-			putchar("32");
+			putchar(44);
+			putchar(32);
 		}
 	}
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
