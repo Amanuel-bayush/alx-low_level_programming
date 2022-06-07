c929e974a98df546114602b3682f612949689f13
Amanuel-bayush <bantelaybr@gmail.com>
2022-06-06 18:22:06 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,12 +11,15 @@ int main(void)
 
 	for (ch = 48; ch <= 57; ch++)
 	{
-		putchar(ch);
-		putchar(n);
-		if (ch != 57 || n != 57)
+		for (n = 48; n <= 57; n++)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(ch);
+			putchar(n);
+			if (ch != 57 || n != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 	}
 	putchar(10);
