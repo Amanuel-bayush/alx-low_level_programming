dd1c1a7391f7319d7c16c905bca3fb514ec60153
Amanuel-bayush <bantelaybr@gmail.com>
2022-06-08 14:46:19 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	int j;
+	for (j = 'a'; j <= 'z'; J++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
